b1f3c070e8afce9f86120d043fd5328138100ef3
Travis Pooley <Travispooley@gmail.com>
2025-07-19 20:25:43 +0930

    launch website 3.0 with initial blog content
    Initial launch of the new website (v3.0).
    Includes a basic landing page and 4 blog posts to start.
    This is the first version with real, usable content
    More design, features, and articles will follow in future updates.

---
+++ assets/js/data/swconf.js
@@ -0,0 +1,15 @@
+const swconf = {
+    cacheName: 'chirpy-1752921956',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+        '/categories/',
+        '/tags/',
+        '/archives/',
+        '/books/',
+    ],
+    interceptor: {paths: [
+      ],urlPrefixes: [
+      ]
+    },
+    purge: false
+};
